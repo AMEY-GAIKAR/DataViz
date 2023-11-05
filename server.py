ce8511898887966691254684fc8f1321e9cfbd82
Anikerur <139677061+Anikerur@users.noreply.github.com>
2023-11-05 18:12:56 +0000

    css commit

--- server.py
+++ server.py
@@ -2,109 +2,96 @@ import pandas as pd
 import seaborn as sns
 import plotly.express as px
 from dash import Dash, dcc, html, dash_table, callback, Output, Input
-import dash_mantine_components as dmc
+import dash_bootstrap_components as dbc
 
-app = Dash(__name__)
+external_stylesheets = [dbc.themes.BOOTSTRAP, "https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"]
 
-# df = pd.read_csv('')
+app = Dash(__name__, external_stylesheets=external_stylesheets)
+
 df = sns.load_dataset('penguins')
 
-colors = {
-    'background': '#111111',
-    'text': '#7FDBFF'
-}
-
-def generate_table(dataframe, max_rows=10):
-    return html.Table([
-        html.Thead(
-            html.Tr([html.Th(col) for col in dataframe.columns])
+# Adding a custom background image
+app.layout = html.Div(
+    style={'background-color': '#F3E9D2', 'font-family': 'Poppins, sans-serif', 'height': '100vh'},
+    children=[
+        dbc.Container(
+            [
+                html.H4(children='DataFrame',style={'text-align': 'left', 'color': '#283044', 'margin-top': '20px', 'font-size': '24px', 'font-family': 'Poppins, sans-serif', 'font-weight': 'bold'}),
+                dash_table.DataTable(
+                    data=df.to_dict('records'),
+                    page_size=10,
+                    style_cell={'textAlign': 'left', 'min-width': '150px', 'width': '150px', 'max-width': '150px', 'font-family': 'Arial, sans-serif', 'font-size': '16px', 'color': '#283044'},
+                    style_header={'backgroundColor': '#F7D6BF', 'color': '#283044', 'font-family': 'Arial, sans-serif', 'font-size': '18px'},
+                    style_as_list_view=True,
+                ),
+                html.Hr(),
+                html.H4(children='Boxplot', style={'text-align': 'center', 'color': '#283044', 'margin-top': '20px', 'font-size': '24px', 'font-family': 'Poppins, sans-serif', 'font-weight': 'bold'}),
+                dbc.Row(
+                    [
+                        dbc.Col(dcc.Dropdown(options=[{'label': col, 'value': col} for col in df.columns], value=df.columns[0], id='x-axis-column-box', style={'font-size': '16px', 'width': '100%'}), width=4),
+                        dbc.Col(dcc.Dropdown(options=[{'label': col, 'value': col} for col in df.columns], value=df.columns[1], id='y-axis-column-box', style={'font-size': '16px', 'width': '100%'}), width=4),
+                        dbc.Col(dcc.Dropdown(options=[{'label': col, 'value': col} for col in df.columns], value=df.columns[-1], id='color-column-box', style={'font-size': '16px', 'width': '100%'}), width=4),
+                    ]
+                ),
+                html.Div(id='graph-box-container'),
+                html.Hr(),
+                html.H4(children='Scatterplot', style={'text-align': 'center', 'color': '#283044', 'margin-top': '20px', 'font-size': '24px', 'font-family': 'Poppins, sans-serif', 'font-weight': 'bold'}),
+                dbc.Row(
+                    [
+                        dbc.Col(dcc.Dropdown(options=[{'label': col, 'value': col} for col in df.columns], value=df.columns[0], id='x-axis-column-scatter', style={'font-size': '16px', 'width': '100%'}), width=4),
+                        dbc.Col(dcc.Dropdown(options=[{'label': col, 'value': col} for col in df.columns], value=df.columns[1], id='y-axis-column-scatter', style={'font-size': '16px', 'width': '100%'}), width=4),
+                        dbc.Col(dcc.Dropdown(options=[{'label': col, 'value': col} for col in df.columns], value=df.columns[-1], id='color-column-scatter', style={'font-size': '16px', 'width': '100%'}), width=4),
+                    ]
+                ),
+                html.Div(id='graph-scatter-container'),
+                html.Hr(),
+                html.H4(children='Histogram', style={'text-align': 'center', 'color': '#283044', 'margin-top': '20px', 'font-size': '24px', 'font-family': 'Poppins, sans-serif', 'font-weight': 'bold'}),
+                dbc.Row(
+                    [
+                        dbc.Col(dcc.Dropdown(options=[{'label': col, 'value': col} for col in df.columns], value=df.columns[1], id='x-axis-column-hist', style={'font-size': '16px', 'width': '100%'}), width=6),
+                        dbc.Col(dcc.Dropdown(options=[{'label': col, 'value': col} for col in df.columns], value=df.columns[-1], id='color-column-hist', style={'font-size': '16px', 'width': '100%'}), width=6),
+                    ]
+                ),
+                html.Div(id='graph-hist-container'),
+            ],
+            fluid=True,
+            style={'background-color': '#F3E9D2', 'padding': '20px', 'border-radius': '10px'}
         ),
-        html.Tbody([
-            html.Tr([
-                html.Td(dataframe.iloc[i][col]) for col in dataframe.columns
-            ]) for i in range(min(len(dataframe), max_rows))
-        ])
-    ])
+    ]
+)
 
-app.layout = html.Div([
-    html.H4(style={},children='DataFrame'),
-    dash_table.DataTable(data=df.to_dict('records'),page_size=10),
-    html.H4(style={},children='Boxplot'),
-    html.Div([
-        dcc.Dropdown(df.columns, df.columns[0], id='x-axis-column-box')
-    ], style={
 
-    }),
-    html.Div([
-        dcc.Dropdown(df.columns, df.columns[1], id='y-axis-column-box')
-    ], style={
-            
-    }),
-    html.Div([
-        dcc.Dropdown(df.columns, df.columns[-1], id='color-column-box')
-    ], style={
-
-    }),
-    dcc.Graph(id='graph-box'),
-    html.H4(style={},children='Scatterplot'),
-    html.Div([
-        dcc.Dropdown(df.columns, df.columns[0], id='x-axis-column-scatter')
-    ], style={
-
-    }),
-    html.Div([
-        dcc.Dropdown(df.columns, df.columns[1], id='y-axis-column-scatter')
-    ], style={
-
-   }),
-    html.Div([
-        dcc.Dropdown(df.columns, df.columns[-1], id='color-column-scatter')
-    ], style={
-
-    }),
-    dcc.Graph(id='graph-scatter'),
-    html.H4(style={},children='Histogram'),
-    html.Div([
-        dcc.Dropdown(df.columns, df.columns[1], id='x-axis-column-hist')
-    ], style={
-
-   }),
-    html.Div([
-        dcc.Dropdown(df.columns, df.columns[-1], id='color-column-hist')
-    ], style={
-
-    }),
-    dcc.Graph(id='graph-hist'),
-])
-
-@callback(
-    Output('graph-scatter', 'figure'),
+@app.callback(
+    Output('graph-scatter-container', 'children'),
     Input('x-axis-column-scatter', 'value'),
     Input('y-axis-column-scatter', 'value'),
-    Input('color-column-scatter', 'value')
-) 
+    Input('color-column-scatter', 'value'),
+)
 def scatter_plot(x_axis_column, y_axis_column, color=None):
-    fig = px.scatter(df,x=x_axis_column,y=y_axis_column, color=color)
-    return fig
+    fig = px.scatter(df, x=x_axis_column, y=y_axis_column, color=color)
+    fig.update_layout(plot_bgcolor='#F3E9D2')
+    return dcc.Graph(figure=fig)
 
-@callback(
-    Output('graph-box', 'figure'),
+@app.callback(
+    Output('graph-box-container', 'children'),
     Input('x-axis-column-box', 'value'),
     Input('y-axis-column-box', 'value'),
-    Input('color-column-box', 'value')
-) 
+    Input('color-column-box', 'value'),
+)
 def boxplot_plot(x_axis_column, y_axis_column=None, color=None):
-    fig = px.box(df,x=x_axis_column,y=y_axis_column, color=color)
-    return fig
+    fig = px.box(df, x=x_axis_column, y=y_axis_column, color=color)
+    fig.update_layout(plot_bgcolor='#F3E9D2')
+    return dcc.Graph(figure=fig)
 
-@callback(
-    Output('graph-hist', 'figure'),
+@app.callback(
+    Output('graph-hist-container', 'children'),
     Input('x-axis-column-hist', 'value'),
-    Input('color-column-hist', 'value')
-) 
+    Input('color-column-hist', 'value'),
+)
 def histplot_plot(x_axis_column, color=None):
-    fig = px.histogram(df,x=x_axis_column, color=color)
-    return fig
+    fig = px.histogram(df, x=x_axis_column, color=color)
+    fig.update_layout(plot_bgcolor='#F3E9D2')
+    return dcc.Graph(figure=fig)
 
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == '__main__':
+    app.run_server(debug=True)
